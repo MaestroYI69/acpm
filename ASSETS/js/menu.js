b85b064258d0ca090d2acfd3f9a7faa9bbd2fc5e
MaestroYI69 <reservamiguelgomez@gmail.com>
2024-09-04 21:01:51 -0500

    BETTER FUNC

---
+++ ASSETS/js/menu.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+    $('#navbar-container').html("<a href='#' class='navbar-logo'>ACPM</a> <div class='menu-icon' id='menu-icon'> <i class='fas fa-bars'></i></div><ul class='nav-menu'>       <li class='nav-item'><a href='index.html' class='nav-link'>Inicio</a></li>   </ul>")
+ })
